6608a56367cef301878a060ee4d1d8433b79d1e5
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2026-01-22 10:31:08 +0000

    feat(sysinfo_reading): ScheduledProducer && trace logging to GetReading

--- sysinfo/sysinfo_reading.go
+++ sysinfo/sysinfo_reading.go
@@ -1,16 +1,21 @@
 package sysinfo
 
 import (
+	"context"
 	"time"
+
 	"github.com/shirou/gopsutil/v4/load"
 	"github.com/shirou/gopsutil/v4/mem"
+	log "github.com/sirupsen/logrus"
 )
 
 func GetReading() (map[string]float64, error) {
+	log.Tracef("attempting to read virtual memory info")
 	vmemStat, err := mem.VirtualMemory()
 	if err != nil {
 		return nil, err
 	}
+	log.Tracef("attempting to read average load info")
 	amount, err := load.Avg()
 	if err != nil {
 		return nil, err
@@ -25,4 +30,27 @@ func GetReading() (map[string]float64, error) {
 		"load_5":         amount.Load5,
 		"load_15":        amount.Load15,
 	}, nil
+}
+
+func ScheduledProducer(ctx context.Context, messageChannel chan map[string]float64) {
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case t := <-ticker.C:
+			log.Infof("Scheduled sysinfo job running at: %v", t)
+			data, err := GetReading()
+			if err != nil {
+				log.Errorf("error reading sysinfo: %v", err)
+			}
+
+			log.Tracef("writing sysinfo to sysinfo channel")
+			messageChannel <- data
+
+		case <-ctx.Done():
+			log.Infof("Scheduled sysinfo task stopped")
+			return
+		}
+	}
 }
