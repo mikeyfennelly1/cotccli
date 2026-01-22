2fd02e15b8f20e683b38964a6aaaa4d509b1e2e0
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2026-01-22 10:29:31 +0000

    feat(root.go): Add log level option to rootCmd

--- cmd/root.go
+++ cmd/root.go
@@ -4,12 +4,24 @@ import (
 	"fmt"
 	"os"
 
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
+
+var logLevel string
 
 var rootCmd = &cobra.Command{
 	Use:   "sysinfo",
 	Short: "Runs on the host, pushing sysinfo data to the configured collector port.",
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		// Parse and set the log level
+		level, err := log.ParseLevel(logLevel)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Invalid log level: %s\n", logLevel)
+			os.Exit(1)
+		}
+		log.SetLevel(level)
+	},
 }
 
 func Execute() {
@@ -17,4 +29,8 @@ func Execute() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+func init() {
+	rootCmd.PersistentFlags().StringVar(&logLevel, "log-level", "info", "Log level (debug, info, warn, error, fatal, panic)")
 }
