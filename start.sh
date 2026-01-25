d0a5d3e71ee55b8ba0f5d2341db267fb9dc0d7b7
Mikey Fennelly <mikeyp.fennelly@gmail.com>
2026-01-25 23:27:46 +0000

    build(start.sh): far more robust startup script

--- start.sh
+++ start.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-go run main.go start --log-level=debug
+################################################
+# SETUP
+################################################
+OS=$(uname)
+if [[ "$OS" == "Darwin" ]]; then
+	# OSX uses BSD readlink
+	BASEDIR="$(dirname "$0")"
+else
+	BASEDIR=$(readlink -e "$(dirname "$0")/")
+fi
+cd "${BASEDIR}"
+
+source "${BASEDIR}"/../.env
+source "${BASEDIR}"/scripts/helpers.sh
+
+var_must_exist DESKTOP_SYSINFO_LISTEN_PORT DESKTOP_SYSINFO_LOG_LEVEL
+
+go run "${BASEDIR}"/main.go start --log-level="${DESKTOP_SYSINFO_LOG_LEVEL}" --listen_port="${DESKTOP_SYSINFO_LISTEN_PORT}"
